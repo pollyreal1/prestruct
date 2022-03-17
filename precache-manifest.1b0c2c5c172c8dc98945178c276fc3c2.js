7c50cf91d21d17ce84033ac598bc51aebc4f17b6
Paul Real <pollyreal73@gmail.com>
2022-03-17 18:14:56 +0800

    Updates

---
+++ precache-manifest.1b0c2c5c172c8dc98945178c276fc3c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8a2b4bdc67cddcf33991b4a3364e41",
+    "url": "/prestruct/index.html"
+  },
+  {
+    "revision": "969498e391e7d2339531",
+    "url": "/prestruct/static/css/main.38be3fad.chunk.css"
+  },
+  {
+    "revision": "9d0785cfdec06bb6fab4",
+    "url": "/prestruct/static/js/2.f67cb354.chunk.js"
+  },
+  {
+    "revision": "969498e391e7d2339531",
+    "url": "/prestruct/static/js/main.ba4173f4.chunk.js"
+  },
+  {
+    "revision": "5a3e4b917735e2f7f86f",
+    "url": "/prestruct/static/js/runtime-main.95c1bfd9.js"
+  }
+]);
